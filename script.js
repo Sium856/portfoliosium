59d7f40440108e7e84cba0a32e308cd20b51e22c
Jahidul Islam Sium <jahidulislamsium02@gmail.com>
2026-09-25 15:17:19 +0600

    Update script.js

--- script.js
+++ script.js
@@ -344,13 +344,3 @@ function applyTheme(theme) {
     }
   }
 }
-function openSkill(evt, skillId) {
-  const panels = document.querySelectorAll('.skill-panel-content');
-  panels.forEach(panel => panel.classList.remove('active'));
-
-  const tabs = document.querySelectorAll('.skill-tab');
-  tabs.forEach(tab => tab.classList.remove('active'));
-
-  document.getElementById(skillId).classList.add('active');
-  evt.currentTarget.classList.add('active');
-}
